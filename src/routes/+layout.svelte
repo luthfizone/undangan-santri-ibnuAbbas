3466bcc458b56b8315890a4cd79dddf76ec32771
AlfanMask <afanh123123@gmail.com>
2024-06-05 09:23:20 +0700

    feat: setup design system & fontawesome lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
-    import "../app.css";
-  </script>
-  
-  <slot />
+	import '../app.css';
+  import '@fortawesome/fontawesome-free/css/all.min.css'
+</script>
+
+<slot />
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Ingrid+Darling&family=Inika:wght@400;700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        "primary": "#FDFDFB",
+        "secondary": "#232323",
+        "accent": "#508C4C",
+      },
+      backgroundColor: {
+        "accent": "#508C4C",
+      },
+      backgroundImage: {
+        "accent-gradient": "var(--greenish-gradient)",
+      },
+      borderColor: {
+        "accent": "#508C4C",
+      },
+      fontFamily: {
+        'primary': ['Inika', 'serif'],
+        'secondary': ['Montserrat', 'sans-serif'],
+        'accent': ['Ingrid Darling', 'cursive'],
+      }
+    },
   },
   plugins: [],
 }
