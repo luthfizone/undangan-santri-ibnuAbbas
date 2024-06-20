691c0e82c321c0ce0bd9b13d2e5fafc1acf33235
luthfizone <luthfizonedev@gmail.com>
2024-06-20 21:00:26 +0700

    production: final

--- src/stores/rsvp.ts
+++ src/stores/rsvp.ts
@@ -1,31 +1,31 @@
-import { writable } from "svelte/store";
-import type { RSVPConfirmation } from "../constants/rsvp_confirmation";
+import { writable } from 'svelte/store';
+import type { RSVPConfirmation } from '../constants/rsvp_confirmation';
 
-const apiEndpoint = 'http://localhost:3000';
+const apiEndpoint = 'http://api.wisudaibnuabbas.com';
 
 const fetcRSVPs = async (): Promise<RSVPConfirmation[]> => {
-    const response = await fetch(`${apiEndpoint}/rsvps`);
-    return await response.json();
-}
+	const response = await fetch(`${apiEndpoint}/rsvps`);
+	return await response.json();
+};
 
-const createRSVP = async(rsvp: Omit<RSVPConfirmation, 'id'>): Promise<RSVPConfirmation> => {
-    const response = await fetch(`${apiEndpoint}/rsvp`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(rsvp)
-    })
-    return await response.json();
-}
+const createRSVP = async (rsvp: Omit<RSVPConfirmation, 'id'>): Promise<RSVPConfirmation> => {
+	const response = await fetch(`${apiEndpoint}/rsvp`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(rsvp)
+	});
+	return await response.json();
+};
 
 // initialize rsvp datas
 const rsvps = writable<RSVPConfirmation[]>([]);
 const loadRSVPS = async () => {
-    const response = await fetcRSVPs();
-    rsvps.set(response);
-}
+	const response = await fetcRSVPs();
+	rsvps.set(response);
+};
 
 loadRSVPS();
 
-export { rsvps, createRSVP, loadRSVPS }
+export { rsvps, createRSVP, loadRSVPS };
